fdf7907a33acde883079386cbd60e61cf1a07828
wiejooo <jakub.wiejak98@gmail.com>
2023-03-29 21:42:04 +0200

    sporo

--- organizationalAPP/urls.py
+++ organizationalAPP/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from organizationalAPP import views
 
 urlpatterns = [
-    path('main/', views.MainView.as_view(), name='main_page')
+    path('main/', views.MainView.as_view(), name='main_page'),
+    path('wardrobe/', views.WardrobeView.as_view(), name='wardrobe'),
+    path('add/', views.AddClothView.as_view(), name='add')
 ]
--- organizationalAPP/views.py
+++ organizationalAPP/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, FormView
+
+from organizationalAPP.forms import AddForm
+from organizationalAPP.models import Clothes
 
 
 class MainView(View):
@@ -7,3 +11,20 @@ class MainView(View):
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class WardrobeView(ListView):
+    model = Clothes
+    template_name = 'wardrobe.html'
+    context_object_name = 'cloth'
+
+
+class AddClothView(FormView):
+    template_name = 'add.html'
+    form_class = AddForm
+    success_url = '../wardrobe/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+

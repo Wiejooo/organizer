7948cd99b55affcd3d59a168a378734b2428ff96
jwiejak <jakub.wiejak@softax.pl>
2023-04-07 14:49:36 +0200

    css

--- clothes_organiser/settings.py
+++ clothes_organiser/settings.py
@@ -107,6 +107,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = ["static"]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

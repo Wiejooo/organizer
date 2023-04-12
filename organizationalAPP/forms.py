5b9e5f3a0ab607d909f497a5ec95cabda7a60fdc
wiejooo <jakub.wiejak98@gmail.com>
2023-04-12 07:03:18 +0200

    dom

--- organizationalAPP/forms.py
+++ organizationalAPP/forms.py
@@ -7,46 +7,42 @@ from organizationalAPP.models import Clothes, ClothType
 class ClothesForm(ModelForm):
     class Meta:
         model = Clothes
-        fields = ['name', 'brand', 'size', 'description', 'photo']
+        fields = ["name", "brand", "size", "cloth_type", "description", "photo"]
 
 
 class AddForm(forms.ModelForm):
     """Form dodania nowego ubrania"""
 
-
     class Meta:
         model = Clothes
-        fields = ('name', 'brand', 'size', 'cloth_type', 'description', 'photo')
+        fields = ("name", "brand", "size", "cloth_type", "description", "photo")
 
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'brand': forms.TextInput(attrs={'class': 'form-control'}),
-            'size': forms.Select(attrs={'class': 'form-control'}),
-            'cloth_type': forms.Select(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "brand": forms.TextInput(attrs={"class": "form-control"}),
+            "size": forms.Select(attrs={"class": "form-control"}),
+            "cloth_type": forms.Select(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "rows": 4}),
         }
 
     def __init__(self, *args, **kwargs):
         super(AddForm, self).__init__(*args, **kwargs)
-        self.fields['name'].label = "Nazwa"
-        self.fields['brand'].label = "Marka"
-        self.fields['size'].label = "Rozmiar"
-        self.fields['description'].label = "Opis"
-        self.fields['photo'].label = "Zdjęcie"
+        self.fields["name"].label = "Nazwa"
+        self.fields["brand"].label = "Marka"
+        self.fields["size"].label = "Rozmiar"
+        self.fields["description"].label = "Opis"
+        self.fields["photo"].label = "Zdjęcie"
 
 
 class AddTypeForm(forms.ModelForm):
     """Form dodania nowego typu ubrania"""
 
-
     class Meta:
         model = ClothType
-        fields = ('type', )
+        fields = ("type",)
 
-        widgets = {
-            'type': forms.TextInput(attrs={'class': 'form-control'})
-            }
+        widgets = {"type": forms.TextInput(attrs={"class": "form-control"})}
 
     def __init__(self, *args, **kwargs):
         super(AddTypeForm, self).__init__(*args, **kwargs)
-        self.fields['type'].label = "Nowy typ ubrania"
+        self.fields["type"].label = "Nowy typ ubrania"

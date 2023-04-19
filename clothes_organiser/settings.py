864b73ffe09206b428131a2902676badf4acb172
jwiejak <jakub.wiejak@softax.pl>
2023-04-19 16:34:06 +0200

    w pracy

--- clothes_organiser/settings.py
+++ clothes_organiser/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'livereload',
     "django.contrib.staticfiles",
     "organizationalAPP",
     "django_filters"
@@ -37,6 +38,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = "clothes_organiser.urls"
